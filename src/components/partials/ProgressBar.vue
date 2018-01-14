d4a9dbead824a065f8ca6df1a4bbab7e6fa4c9ee
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2018-01-14 17:46:11 +0200

    separate progress bar into a component

---
+++ src/components/partials/ProgressBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="progress-bar">
+    <li v-for="index in questionsLength" :class="{ 'passed': activeId >= index }" :key="index">
+      <span>{{ index }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { returnQuestionsLength } from '@/store';
+export default {
+  props: {
+    activeId: {
+      type: Number | String,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      questionsLength: 0
+    };
+  },
+  created() {
+    this.questionsLength = returnQuestionsLength();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import '../../util/vars';
+.progress-bar {
+    list-style-type: none;
+    width: 100%;
+    display: inline-flex;
+    padding: 0;
+    justify-content: space-between;
+    li {
+        flex-basis: 8%;
+        background-color: $dark;
+        color: $light;
+        transition: 0.3s;
+        &.passed {
+            background-color: $light;
+            color: $dark;
+            position: relative;
+            &:not(:first-child) {
+                &:before {
+                    content: '';
+                    width: 25%;
+                    height: 1px;
+                    background-color: $light;
+                    position: absolute;
+                    left: -26%;
+                    top: 50%;
+                }
+            }
+        }
+    }
+}
+</style>
